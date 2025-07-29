b2d8c5708e2cf62f0d7368fe23deeca99960b1d4
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-07-30 00:04:52 +0530

    Added the new files

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,49 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit as OutfitFont, Ovo as OvoFont, Open_Sans, Poppins, Inter, Roboto, Lato } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const Outfit = OutfitFont({
+  subsets: ["latin"], weight: ["400", "500", "600", "700"]
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const Ovo = OvoFont({
+  subsets: ["latin"], weight: ["400"]
 });
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"], // Add or adjust weights as needed
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"], // Add more if needed
+  variable: "--font-poppins", // Optional: for Tailwind or global CSS
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"], // Add more if needed
+  variable: "--font-inter", // Optional: for Tailwind or global CSS
+});
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"], // Add more weights if needed
+  style: ["normal", "italic"],   // Optional: include italic styles
+  variable: "--font-roboto",     // Optional: for Tailwind or global CSS
+});
+
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["400", "700", "900"], // Add more weights if needed
+  style: ["normal", "italic"],   // Optional: include italic styles
+  variable: "--font-lato",       // Optional: for Tailwind or global CSS
+});
+
+
+
+
+
 
 export const metadata = {
   title: "Portfolio - Vijay",
@@ -18,9 +52,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${lato.className} antialiased`}
       >
         {children}
       </body>
--- app/page.js
+++ app/page.js
@@ -1,6 +1,17 @@
+"use client";
+import About from "./components/About";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
 
 export default function Home() {
   return (
-   <></>
+    <>
+      
+      <Navbar />
+      <Header />
+      <About />
+      <Skills />
+    </>
   );
 }
